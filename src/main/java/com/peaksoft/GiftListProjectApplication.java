1c3df80a2133549b0a4fe44d242b94c4d02e626a
My Name <tadzhibaevaelnura@gmail.com>
2022-12-13 16:52:36 +0300

    first commit

---
+++ src/main/java/com/peaksoft/GiftListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.peaksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftListProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftListProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peaksoft/GiftListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaksoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftListProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
