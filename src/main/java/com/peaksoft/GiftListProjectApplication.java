89b06837306ebee105024651f8baace7026e095c
My Name <tadzhibaevaelnura@gmail.com>
2022-12-19 19:10:06 +0300

    Commit2

--- src/main/java/com/peaksoft/GiftListProjectApplication.java
+++ src/main/java/com/peaksoft/GiftListProjectApplication.java
@@ -2,12 +2,19 @@ package com.peaksoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GiftListProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GiftListProjectApplication.class, args);
+        System.out.println("Welcome collegues, project name is Giftlist-M3!");
+    }
+
+    @GetMapping("/")
+    public String greatingPage(){
+        return "<h1> Welcome to iftlist-M3 Application! <h1>";
     }
 
 }
