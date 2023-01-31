18586fd4f6af7835000ccbe23603e3af32f5747e
Тимур <kadyrbekovtimur001@gmail.com>
2023-01-31 16:48:30 +0600

    commit - third

--- src/main/java/com/peaksoft/dto/ValidationType.java
+++
@@ -1,8 +0,0 @@
-package com.peaksoft.dto;
-
-public class ValidationType {
-
-    public static final String SUCCESSFUL = "successful";
-    public static final String LOGIN_FAILED = "username or password is incorrect";
-
-}
