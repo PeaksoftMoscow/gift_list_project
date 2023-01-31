9eaf99cbd0c445a08393d913e14f1c1ea98496e4
Рыскелди <ryskeldi.osmonaliev@mail.ru>
2023-01-31 22:22:25 +0300

    commit - first

--- src/main/java/com/peaksoft/model/entity/Holiday.java
+++ src/main/java/com/peaksoft/model/entity/Holiday.java
@@ -2,11 +2,9 @@ package com.peaksoft.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.peaksoft.model.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -20,28 +18,34 @@ import java.util.List;
 @AllArgsConstructor
 public class Holiday {
 
-    @Id
-    @GeneratedValue(generator = "holiday_gen",strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "holiday_gen",sequenceName = "holiday_seq",allocationSize = 1)
-    private Long id;
+@Id
+@GeneratedValue(generator = "holiday_gen", strategy = GenerationType.SEQUENCE)
+@SequenceGenerator(name = "holiday_gen", sequenceName = "holiday_seq", allocationSize = 1)
+private Long id;
 
-    private String holidayName;
+private String holidayName;
 
-    private String image;
+private String image;
 
-    @JsonFormat(pattern = "yyyy.MM.dd")
-    private LocalDate date;
+@JsonFormat(pattern = "yyyy.MM.dd")
+private LocalDate date;
 
-    @OneToMany(cascade = {CascadeType.MERGE,
-            CascadeType.PERSIST,
-            CascadeType.REFRESH,
-            CascadeType.DETACH})
-    @JsonIgnore
-    private List<WishList> wishList;
+@OneToMany(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST,
+			CascadeType.MERGE})
+@JoinTable(name = "holiday_wishList", joinColumns = @JoinColumn(name = "holidays_id")
+			, inverseJoinColumns = @JoinColumn(name = "wishList_id"))
+@JsonIgnore
+private List<WishList> wishList;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    @JsonIgnore
-    private User user;
+@Transient
+private Long wish_id;
+
+@ManyToOne
+@JoinColumn(name = "user_id")
+@JsonIgnore
+private User user;
+@Transient
+private Long user_id;
+
 
 }
