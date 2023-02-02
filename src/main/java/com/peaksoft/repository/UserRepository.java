e780120455afad9644374a82db16f6bcf152226d
Тимур <kadyrbekovtimur001@gmail.com>
2023-02-02 12:34:13 +0600

    commit - init

--- src/main/java/com/peaksoft/repository/UserRepository.java
+++ src/main/java/com/peaksoft/repository/UserRepository.java
@@ -2,11 +2,21 @@ package com.peaksoft.repository;
 
 import com.peaksoft.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+
+
+    @Query("SELECT f FROM User u JOIN u.friends f WHERE u.id=?1")
+    List<User> findAllFriends(Long id);
+
+    @Query("SELECT R FROM User u JOIN u.requestToFriends R WHERE u.id=?1")
+    List<User> findAllRequest(Long id);
+
 }
