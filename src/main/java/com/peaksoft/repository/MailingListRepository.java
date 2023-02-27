162274d6edc200a5bb62bb933b2747dc19db3426
imarov11 <saidibakaskambarbekovich@gmail.com>
2023-02-27 22:32:50 +0600

    commit3

--- src/main/java/com/peaksoft/repository/MailingListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.peaksoft.repository;
-
-import com.peaksoft.model.entity.MailingList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MailingListRepository extends JpaRepository<MailingList ,Long> {
-}
