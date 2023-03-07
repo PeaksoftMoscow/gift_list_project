ba79c7b92df8cd91cddf3b7435a921450fad3f2e
atabekdosbaev07 <dosbaeatabek07@gmail.com>
2023-03-07 17:31:59 +0300

    commit 5

--- src/main/java/com/peaksoft/controller/AutController.java
+++ src/main/java/com/peaksoft/controller/AutController.java
@@ -1,16 +1,13 @@
 package com.peaksoft.controller;
 
-import com.peaksoft.dto.UserRequest;
-import com.peaksoft.model.entity.Booking;
-import com.peaksoft.service.*;
-import com.peaksoft.dto.UserResponse;
-import com.peaksoft.dto.ValidationType;
-import com.peaksoft.repository.UserRepository;
+import com.peaksoft.service.BookingServise;
+import com.peaksoft.config.jwt.JwTokenUtil;
 import com.peaksoft.dto.*;
 import com.peaksoft.model.User;
-import com.peaksoft.config.jwt.JwTokenUtil;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.peaksoft.model.entity.Booking;
+import com.peaksoft.repository.UserRepository;
+import com.peaksoft.service.ResetPasswordService;
+import com.peaksoft.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +16,37 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/jwt")
 public class AutController {
 
-    private final UserService userService;
-    private final UserRepository userRepository;
-    private final JwTokenUtil jwTokenUtil;
     private final LoginMapper loginMapper;
 
-    private final BookingServise bookingServise;
+    private final JwTokenUtil jwTokenUtil;
+
+    private final UserRepository userRepository;
+
     private final AuthenticationManager authenticationManager;
 
+    private final UserService userService;
+
+    private final ResetPasswordService resetPasswordService;
+    private final BookingServise bookingServise;
+
+
     @PostMapping("login")
-    public ResponseEntity<LoginResponse> getLogin(@RequestBody UserRequest request){
+    public ResponseEntity<LoginResponse> getLogin(@RequestBody UserRequest request) {
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(request.getEmail(),
                     request.getPassword());
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
             User user = userRepository.findByEmail(token.getName()).get();
             return ResponseEntity.ok().body(loginMapper.loginView(jwTokenUtil.generateToken(user), ValidationType.SUCCESSFUL, user));
-        }catch (BadCredentialsException ex){
+        } catch (BadCredentialsException ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(loginMapper.loginView("", ValidationType.LOGIN_FAILED, null));
 
         }
@@ -57,8 +63,7 @@ public class AutController {
     }
 
     @PostMapping("bookWish/{id}")
-
-    public String bokinhWislist(@PathVariable Long id) {
+    public String bookingWishlist(@PathVariable Long id) {
         return bookingServise.bookWishlist(id);
     }
 }
